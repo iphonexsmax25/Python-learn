3ece3c4af55f281a7b87e72b69f728ea2782b285
iphonexsmax25 <limisaac2022@gmail.com>
2024-10-10 21:34:14 +0800

    Add files via upload

---
+++ Lesson_4_reference.py
@@ -0,0 +1,64 @@
+# case sturcture
+
+#def day_of_the_week(day):
+    #match day: 
+       #case "Monday":
+          # print("Mon")
+       #case "Tuesday":
+           # print("Tue")
+       #case "Wednesday":
+           # print("Wed")
+       #case "Thursday":
+            #print("Thu")
+       #case "Friday":
+           #print ("Fri")
+       #case "Saturday":
+           #print ("Sat")
+       #case "Sunday":
+           #print ("Sun")
+       #case _:
+           #print("Invalid")
+
+# print (day_of_the_week("Sunday"))
+
+
+
+
+#if structure
+
+
+#temperature = float(input("Room Temperature now:"))
+
+#if temperature < 40:
+ #   print("Is cold outside")
+  #  print("Get a coat")
+#else:
+ #   print("Is still warm")
+
+
+
+#age = int(input("Enter your age: "))
+
+
+#if age >= 17:
+ #   print("you are legal to have driving license")
+#else:
+ #   print("you can't have driving license ")
+  #  print(" and you are not legible to drive ")
+   
+  
+
+
+#if else if  and else 
+#age = int(input("Enter your age:"))
+#if age <= 12:
+ #   print("Kid")
+# elif age <= 18:
+ #   print("Teenager")
+ # elif age <= 59:
+   # print("Adult")
+#else:
+ #   print("Senior Citizen")
+
+
+
