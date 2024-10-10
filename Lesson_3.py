704c9a280c1e19bb913063a6575e602d58a6442d
iphonexsmax25 <limisaac2022@gmail.com>
2024-10-10 21:39:17 +0800

    Add files via upload

---
+++ Lesson_3.py
@@ -0,0 +1,81 @@
+﻿
+#print(20>10)
+#print(10 == 10)
+#print(20<10)
+
+#print(False or False)
+#print(False and False)
+#print( not True)
+
+
+
+#x = 900000000
+#y = 1000000
+#if y > x:
+    #print(" 'Y' is greater than 'X'")
+#else:
+    #print("'Y' is lesser than 'X'")
+
+
+
+    #list format and method
+
+#mylist = [2, 1, 3, 5, 10, 8, 9, 4, 6, 7,]
+#mylist.sort()
+#print(mylist)
+#print(min(mylist))
+#print(max(mylist))
+#print(len(mylist))
+
+# the repition operator
+
+#list =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#for x in list:
+ #print(x)
+
+
+
+
+ #the list operator
+
+
+#list =  [1, 2, 3, 4, 5, 6] + [7]
+#list2 =  [1, 2, 3, 4, 5, 6] * 2
+#list3 =  7 in [1, 2, 3, 4, 5, 6] 
+#list4 = [1, 2, 3, 4, 5, 6] < [10, 20, 30, 40, 50, 60]
+#print(list)
+#print(list2)
+#print(list3)
+#print(list4)
+
+# the repition operator
+
+#list =  [1, 2, 3, 4, 5, 6]
+#for x  in list:
+ #print(x)
+
+ # the list dictionary
+
+#list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+#print(list[0:7])
+
+#the Nested List
+
+#nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#print(nested_list[2][2])
+
+#mutable list
+
+#mylist = [1, 2, 3, 4, 5, 6]
+#mylist[0] = 130
+#print(mylist)
+
+
+# dictionaries
+
+#this_dict = { "blue":"biru", "green":"hijau", "red":"merah"}
+#print(this_dict)
+
+
+
+
